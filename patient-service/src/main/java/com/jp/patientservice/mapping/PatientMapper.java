e158a861a7d057a0d0e860a3ba41f298c394ee30
PortillaXpert <jrivera082002@gmail.com>
2025-03-22 23:20:58 -0500

    Implement patientService requestDTO

--- patient-service/src/main/java/com/jp/patientservice/mapping/PatientMapper.java
+++ patient-service/src/main/java/com/jp/patientservice/mapping/PatientMapper.java
@@ -1,7 +1,10 @@
 package com.jp.patientservice.mapping;
 
+import com.jp.patientservice.dto.PatientRequestDTO;
 import com.jp.patientservice.dto.PatientResponseDTO;
 import com.jp.patientservice.models.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
     public static PatientResponseDTO toDTO(Patient patient) {
@@ -14,5 +17,14 @@ public class PatientMapper {
         return patientDTO;
     }
 
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
+    }
 
 }
--- patient-service/src/main/java/com/jp/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/jp/patientservice/service/PatientService.java
@@ -1,5 +1,6 @@
 package com.jp.patientservice.service;
 
+import com.jp.patientservice.dto.PatientRequestDTO;
 import com.jp.patientservice.dto.PatientResponseDTO;
 import com.jp.patientservice.mapping.PatientMapper;
 import com.jp.patientservice.models.Patient;
@@ -21,5 +22,12 @@ public class PatientService {
         List<Patient> patients = patientRepository.findAll();
         return patients.stream()
                 .map(PatientMapper::toDTO).toList();
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+    Patient newPatient = patientRepository.save(
+            PatientMapper.toModel(patientRequestDTO));
+
+    return PatientMapper.toDTO(newPatient);
     }
 }
