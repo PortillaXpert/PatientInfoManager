b9dfcca81b9c14aee5026a2d8753118c813b0f45
PortillaXpert <jrivera082002@gmail.com>
2025-05-26 00:27:14 -0500

    Configure Dockerfile of api-gateway.

---
+++ api-gateway/src/test/java/com/jp/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
